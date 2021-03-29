db62394c0d991835cb75367f74df6cfaa5109487
Harrison-Berek <hcberek@gmail.com>
2021-03-28 18:28:46 -0600

    Home Page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('home');
 });
 
 router.get('/auth/google', passport.authenticate(
